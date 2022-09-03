2d068ef63594312079c46057d68da169a37ee741
lady-araminta <jparkhomenko@gmail.com>
2022-09-03 21:23:51 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listRef = document.querySelector('ul');
+
+const ingredientsRef = ingredients.map(ingredient => {
+  const listItemRef = document.createElement('li');
+  listItemRef.textContent = ingredient;
+  listItemRef.classList.add('item');
+  return listItemRef;
+});
+
+listRef.append(...ingredientsRef);
