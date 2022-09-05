43a29b5c67307c763030ec8fde3300ebad013180
lady-araminta <jparkhomenko@gmail.com>
2022-09-05 16:30:03 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,15 @@
+let counterValue = 0;
+
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const value = document.querySelector('#value');
+
+incrementBtn.addEventListener('click', () => {
+  counterValue += 1;
+  value.textContent = counterValue;
+});
+
+decrementBtn.addEventListener('click', () => {
+  counterValue -= 1;
+  value.textContent = counterValue;
+});
