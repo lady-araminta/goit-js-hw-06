915eb557525a77db59a7393cc4ac740e7348374f
lady-araminta <jparkhomenko@gmail.com>
2022-09-08 20:23:31 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
-
+const formRef = document.querySelector('.login-form');
+const submitHandler = event => {
+  event.preventDefault();
+  if (event.target.elements.email.value === '' || event.target.elements.password.value === '') {
+    alert('Заповніть, будь ласка, усі поля!');
+  }
+  const userObject = {
+    email: event.target.elements.email.value,
+    password: event.target.elements.password.value,
+  };
+  console.log(userObject);
+  event.currentTarget.reset();
+};
+formRef.addEventListener('submit', submitHandler);
