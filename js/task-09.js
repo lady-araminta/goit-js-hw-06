998ae222ffece6bb70ff53e28cda86fbefb43337
lady-araminta <jparkhomenko@gmail.com>
2022-09-09 16:42:28 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const bodyRef = document.querySelector('body');
+const colorDescrRef = document.querySelector('.color');
+const colorChangeBtn = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function getColorChange() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorDescrRef.textContent = bodyRef.style.backgroundColor;
+}
+
+colorChangeBtn.addEventListener('click', getColorChange);
